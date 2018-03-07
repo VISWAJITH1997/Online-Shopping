7da2e8706b266f231cd561863d1e7f750c4797c0
MAVULURU VISWAJITH REDDY <30382044+VISWAJITH1997@users.noreply.github.com>
2018-03-07 15:02:13 +0530

    Static FrontEnd

--- onlineshopping/src/main/java/net/visu/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/visu/onlineshopping/controller/PageController.java
@@ -1,16 +1,27 @@
 package net.visu.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
 
-	@RequestMapping(value= {"/", "/home", "/index"})
+	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC");	
+		mv.addObject("greeting", "Welcome to My Website");
 		return mv;
-	}	
+	}
+
+	@RequestMapping(value = "/test{greeting}")
+	public ModelAndView test(@PathVariable("greeting") String greeting) {
+		if (greeting == null) {
+			greeting = "hello world";
+		}
+		ModelAndView mv = new ModelAndView("book");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
 }
